b03b19b77839b9b77086e198e19afbcba469ab68
ShivamGupta15347 <shivamgupta15347@gmail.com>
2023-10-06 19:03:42 +0530

    s

--- src/App.js
+++ src/App.js
@@ -4,10 +4,44 @@ import Item from "./components/Item";
 import ItemDate from "./components/ItemDate";
 
 function App() {
+  const response = [
+    { itemName: "Nirma", ItemDate: "20", itemMonth: "June", itemYear: "1998" },
+
+    {
+      itemName: "Nirma2",
+      ItemDate: "20",
+      itemMonth: "June2",
+      itemYear: "1998",
+    },
+
+    {
+      itemName: "Nirma3",
+      ItemDate: "20",
+      itemMonth: "June3",
+      itemYear: "1998",
+    },
+  ];
   return (
     <div>
-      <Item></Item>
-      <ItemDate></ItemDate>
+      <Item name={response[0].itemName}>Hello jee mai hu john the don</Item>
+      <ItemDate
+        day={response[0].ItemDate}
+        month={response[0].itemMonth}
+        year={response[0].itemYear}
+      ></ItemDate>
+      <Item name={response[1].itemName}></Item>
+      <ItemDate
+        day={response[1].ItemDate}
+        month={response[1].itemMonth}
+        year={response[1].itemYear}
+      ></ItemDate>
+
+      <Item name={response[1].itemName}></Item>
+      <ItemDate
+        day={response[1].ItemDate}
+        month={response[1].itemMonth}
+        year={response[1].itemYear}
+      ></ItemDate>
       <div className="App">Hello Jee</div>
     </div>
   );
