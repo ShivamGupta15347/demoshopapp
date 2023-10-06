dd002652eeaf669e1ae3e7eee1b5507170093d4f
ShivamGupta15347 <shivamgupta15347@gmail.com>
2023-10-06 18:23:12 +0530

    started

---
+++ src/components/ItemDate.js
@@ -0,0 +1,16 @@
+import "./ItemDate.css";
+
+function ItemDate() {
+  const day = 20;
+  const month = "June";
+  const year = 1998;
+  return (
+    <div className="mfg-Date">
+      <span>{day}</span>
+      <span> {month}</span>
+      <span> {year}</span>
+    </div>
+  );
+}
+
+export default ItemDate;
